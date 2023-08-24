5da5c6229a0cd2219adca918d80e38507d14e082
Zccc <ryanzhanemail@qq.com>
2023-08-24 12:14:05 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+模板来自https://www.bilibili.com/video/BV17u411n7t8/?spm_id_from=333.999.0.0&vd_source=0960d0288b3cf1cfec8c0eb9e1ba322e
+时间戳：04:12
---
+++ scripts/preprocess.py
@@ -0,0 +1,10 @@
+import os
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+
+def hello_world():
+    print("Hello World")
+
